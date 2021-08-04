dfb39ca85f5b40ba10fe69f03e32324f4e5bf60c
Albert Kuc <albert.kuc@gmail.com>
2021-08-04 15:11:28 +0100

    fear: Add playoff and select all in basketball_app
    Add season/playoff radio button
    Update load_data() with season/playoff parameter
    
    Add select all checkbox to Teams
    Add select all checkbox to Positions

--- basketball_app.py
+++ basketball_app.py
@@ -7,17 +7,18 @@ import numpy as np
 
 
 @st.cache
-def load_data(year: int) -> pd.DataFrame:
+def load_data(year: int, stage: str) -> pd.DataFrame:
     """
     Web scraping of NBA player stats
 
     Args:
         year (int):
+        stage (str):
 
     Returns:
         pd.DataFrame
     """
-    url = ''.join(('https://www.basketball-reference.com/leagues/NBA_', str(year), '_per_game.html'))
+    url = ''.join(('https://www.basketball-reference.com/', stage, '/NBA_', str(year), '_per_game.html'))
     html = pd.read_html(url, header=0)
     df = html[0]
     # drop rows with recursive header values
@@ -42,7 +43,8 @@ def download_csv_file_link(df: pd.DataFrame) -> str:
         href str
     """
     csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # string <-> bytes conversions
+    # string <-> bytes conversions
+    b64 = base64.b64encode(csv.encode()).decode()
     href = f'<a href="data:file/csv;base64,{b64}" download="player_stats.csv">Download CSV File</a>'
     return href
 
@@ -72,17 +74,37 @@ def generate_heatmap(df: pd.DataFrame):
 st.sidebar.header('User Input Features')
 selected_year_int = st.sidebar.selectbox(label='Year', options=list(reversed(range(1950, 2022))))
 
+# Sidebar - Stage
+radio_input = st.sidebar.radio('Season/Play-off', ['Season', 'Play-off'])
+if radio_input == 'Season':
+    stage_str = 'leagues'
+else:
+    stage_str = 'playoffs'
+
 # Load data with selected Year
-player_stats_df = load_data(selected_year_int)
+player_stats_df = load_data(selected_year_int, stage_str)
 
 # Sidebar - Team selection
 sorted_unique_team = sorted(player_stats_df.Tm.unique())
-selected_team = st.sidebar.multiselect(label='Team', options=sorted_unique_team, default=sorted_unique_team,
-                                       help='Display selected teams stats')
+container_team = st.sidebar.beta_container()
+all_teams = st.sidebar.checkbox("Select all teams", value=True)
+
+if all_teams:
+    selected_team = container_team.multiselect(label='Team', options=sorted_unique_team, default=sorted_unique_team,
+                                               help='Display selected teams stats')
+else:
+    selected_team = container_team.multiselect(label='Team', options=sorted_unique_team,
+                                               help='Display selected teams stats')
 
 # Sidebar - Position selection
 unique_pos = ['C', 'PF', 'SF', 'PG', 'SG']
-selected_pos = st.sidebar.multiselect('Position', unique_pos, unique_pos)
+container_pos = st.sidebar.beta_container()
+all_pos = st.sidebar.checkbox("Select all positions", value=True)
+
+if all_pos:
+    selected_pos = container_pos.multiselect('Position', unique_pos, unique_pos)
+else:
+    selected_pos = container_pos.multiselect('Position', unique_pos)
 
 # Filtering data
 df_selected_team_and_position = player_stats_df[(player_stats_df.Tm.isin(selected_team)) &
